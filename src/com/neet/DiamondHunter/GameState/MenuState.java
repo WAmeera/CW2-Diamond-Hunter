9e60f0e07d50e29bbc92fc88507727fe272560c9
WAmeera <hcyww1@nottingham.edu.my>
2018-12-19 00:09:13 +0800

    improved flow from maingame to viewmap and back to main game for better UX Signed-off-by: WAmeera <hcyww1@nottingham.edu.my>

--- src/com/neet/DiamondHunter/GameState/MenuState.java
+++ src/com/neet/DiamondHunter/GameState/MenuState.java
@@ -2,96 +2,100 @@
 
 package com.neet.DiamondHunter.GameState;
 
-import Mapviewer.Start;
-import com.neet.DiamondHunter.Main.Game;
 import com.neet.DiamondHunter.Manager.Content;
 import com.neet.DiamondHunter.Manager.GameStateManager;
 import com.neet.DiamondHunter.Manager.JukeBox;
 import com.neet.DiamondHunter.Manager.Keys;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 
-public class MenuState extends GameState {
-	
-	private BufferedImage bg;
-	private BufferedImage diamond;
-	
+public class MenuState extends com.neet.DiamondHunter.GameState.GameState {
+
+	public static int menupage_state = 0;
+	private java.awt.image.BufferedImage bg;
+	private java.awt.image.BufferedImage diamond;
 	private int currentOption = 0;
 	private String[] options = {
 		"PLAY",
 		"EDIT MODE",
 		"QUIT"
 	};
-	
-	public MenuState(GameStateManager gsm) {
+    
+
+	public MenuState(com.neet.DiamondHunter.Manager.GameStateManager gsm) {
 		super(gsm);
 	}
 	
 	public void init() {
-		bg = Content.MENUBG[0][0];
-		diamond = Content.DIAMOND[0][0];
-		JukeBox.load("/SFX/collect.wav", "collect");
-		JukeBox.load("/SFX/menuoption.wav", "menuoption");
+		bg = com.neet.DiamondHunter.Manager.Content.MENUBG[0][0];
+		diamond = com.neet.DiamondHunter.Manager.Content.DIAMOND[0][0];
+		com.neet.DiamondHunter.Manager.JukeBox.load("/SFX/collect.wav", "collect");
+		com.neet.DiamondHunter.Manager.JukeBox.load("/SFX/menuoption.wav", "menuoption");
 	}
 	
 	public void update() {
 		handleInput();
 	}
 	
-	public void draw(Graphics2D g) {
-		
-		g.drawImage(bg, 0, 0, null);
-		
-		Content.drawString(g, options[0], 48, 90);
-		Content.drawString(g, options[1], 28, 105);
-		Content.drawString(g, options[2], 48, 120);
-		
-		if(currentOption == 0) g.drawImage(diamond, 25, 86, null);
-		else if(currentOption == 1) g.drawImage(diamond, 8, 101, null);
-		else if(currentOption == 2) g.drawImage(diamond, 25, 116, null);
+	public void draw(java.awt.Graphics2D g) {
+			g.drawImage(bg, 0, 0, null);
+			if (menupage_state == 0) {
+				com.neet.DiamondHunter.Manager.Content.drawString(g, options[0], 48, 90);
+				com.neet.DiamondHunter.Manager.Content.drawString(g, options[1], 28, 105);
+				com.neet.DiamondHunter.Manager.Content.drawString(g, options[2], 48, 120);
 
-		
+                if (currentOption == 0) g.drawImage(diamond, 25, 86, null);
+                else if (currentOption == 1) g.drawImage(diamond, 8, 101, null);
+                else if (currentOption == 2) g.drawImage(diamond, 25, 116, null);
+			}
+			else if (menupage_state == 1) {
+				com.neet.DiamondHunter.Manager.Content.drawString(g, options[0], 48, 90);
+				com.neet.DiamondHunter.Manager.Content.drawString(g, options[2], 48, 105);
+
+                if (currentOption == 0) g.drawImage(diamond, 25, 86, null);
+                else if (currentOption == 1) g.drawImage(diamond, 25, 101, null);
+			}
+
+
 	}
 	
 	public void handleInput() {
-		if(Keys.isPressed(Keys.DOWN) && currentOption < options.length - 1) {
-			JukeBox.play("menuoption");
-			currentOption++;
-		}
-		if(Keys.isPressed(Keys.UP) && currentOption > 0) {
-			JukeBox.play("menuoption");
+	   if (com.neet.DiamondHunter.Manager.Keys.isPressed(com.neet.DiamondHunter.Manager.Keys.DOWN) && currentOption < options.length - 1) {
+	        com.neet.DiamondHunter.Manager.JukeBox.play("menuoption");
+	        currentOption++;
+	    }
+
+		if(com.neet.DiamondHunter.Manager.Keys.isPressed(com.neet.DiamondHunter.Manager.Keys.UP) && currentOption > 0) {
+			com.neet.DiamondHunter.Manager.JukeBox.play("menuoption");
 			currentOption--;
 		}
-		if(Keys.isPressed(Keys.ENTER)) {
-			JukeBox.play("collect");
+		if(com.neet.DiamondHunter.Manager.Keys.isPressed(com.neet.DiamondHunter.Manager.Keys.ENTER)) {
+			com.neet.DiamondHunter.Manager.JukeBox.play("collect");
 			selectOption();
 		}
 	}
 	
 	private void selectOption() {
-		if(currentOption == 0) {
-			gsm.setState(GameStateManager.PLAY);
+		if (currentOption == 0) {
+			gsm.setState(com.neet.DiamondHunter.Manager.GameStateManager.PLAY);
 
 		}
-		if(currentOption == 1){
+		if (currentOption == 1) {
 
-
-
-			/*Handles exception where map does not open except once
-			 and prevents Edit map open more than once
-			*/
-			try {
-					Start.main();
-					Game.window.setVisible(false);
-				} catch (IllegalStateException e) {
-				}
-
-
+            try{
+                com.neet.DiamondHunter.Main.Game.window.setVisible(false);
+                Mapviewer.Start.main();
+            }catch (IllegalStateException e)
+            {
+                System.exit(0);
+            }
 
 		}
-		if(currentOption == 2) {
+		if (currentOption == 2) {
 			System.exit(0);
+
 		}
 	}
 
